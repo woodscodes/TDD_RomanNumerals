0af09cab938f13ff8f51a5b704edf692c8a06a0c
stuwo <stooberto@gmail.com>
2020-08-13 08:59:18 +0900

    Add project files.

---
+++ TDD_RomanNumerals.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TDD_RomanNumerals.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
