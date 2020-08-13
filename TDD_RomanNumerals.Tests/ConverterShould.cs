b18e8b1237e54aa782e20974bf77d7e41c8f69d9
stuwo <stooberto@gmail.com>
2020-08-13 09:13:29 +0900

    First test and some generated classes

---
+++ TDD_RomanNumerals.Tests/ConverterShould.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace TDD_RomanNumerals.Tests
+{
+    public class ConverterShould
+    {
+        [Fact]
+        public void Convert1intoI()
+        {
+            var result = NumeralConverter.Convert(1);
+
+            Assert.Equal("I", result);
+        }
+    }
+}
---
+++ TDD_RomanNumerals.Tests/NumeralConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TDD_RomanNumerals.Tests
+{
+    internal class NumeralConverter
+    {
+        internal static string Convert(int n)
+        {
+            if (n == 1)
+                return "I";
+
+            throw new InvalidOperationException();
+        }
+    }
+}
